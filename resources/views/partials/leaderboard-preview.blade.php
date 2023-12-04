20018efd7a29cee35a4550eda9eb66bcc59ab24b
Firda Rosiana T <firdaroziana10@gmail.com>
2023-12-04 21:59:04 +0700

    modify index home for leaderboard-partials

--- resources/views/partials/leaderboard-preview.blade.php
+++ resources/views/partials/leaderboard-preview.blade.php
@@ -5,19 +5,23 @@
     </div>
 
     <div class="col-lg-5 ml-auto">
-        <div class="card border">
+        <div class="card border rounded-5 bg-danger bg-gradient">
             <div class="card-body">
-                <div class="text-center">
-                    <h3 class="card-title py-2"><i class="fa-solid fa-medal text-danger"></i> Leaderboard <i class="fa-solid fa-medal text-danger"></i></h3>
+                <div class="text-center text-white">
+                    <h3 class="card-title py-2"><i class="fa-solid fa-medal"></i> Leaderboard <i class="fa-solid fa-medal"></i></h3>
                 </div>
 
                 {{-- <p class="card-text">Peringkat teratas metricfy</p> --}}
                 <ul class="list-group list-group-flush">
                     @foreach ($leaderboard as $item)
-                        <li class="list-group-item">
-                            <div class="d-flex justify-content-between">
-                                <div class="list-name">
-                                    {{ $item->name }}</div>
+                        <li class="list-group-item rounded-3 bg-body-tertiary mb-2">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="d-flex justify-content-start align-items-center">
+                                <div class="list-ava">
+                                    <img src="{{ asset('img/avatar/' . $item->avatar) }}" class="rounded-circle" width="36px" height="36px" alt="">
+                                </div>
+                                <div class="list-name ps-3">
+                                    {{ $item->name }}</div></div>
                                 <div class="list-point">
                                     <span class="badge badge-pill badge-primary"><i class="fa-solid fa-bolt text-warning"></i> {{ $item->point }} XP</span>
                                 </div>
